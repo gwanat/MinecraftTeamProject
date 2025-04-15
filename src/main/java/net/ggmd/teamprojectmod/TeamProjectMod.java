93c04efc57910c9db52bc357fea103f26b3e7a0e
MichalWojnar0 <michal.wojnar@onet.pl>
2025-04-15 19:56:50 +0200

    raw brick ingridient added to game and to creative menu

--- src/main/java/net/ggmd/teamprojectmod/TeamProjectMod.java
+++ src/main/java/net/ggmd/teamprojectmod/TeamProjectMod.java
@@ -1,6 +1,8 @@
 package net.ggmd.teamprojectmod;
 
 import com.mojang.logging.LogUtils;
+import net.ggmd.teamprojectmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +31,8 @@ public class TeamProjectMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -45,7 +49,9 @@ public class TeamProjectMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.RAW_BRICK);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
