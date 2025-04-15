faafc301cc5f1a64222cc7e73d8019b92c90fbb2
MichalWojnar0 <michal.wojnar@onet.pl>
2025-04-15 19:56:50 +0200

    raw brick ingridient added to game and to creative menu

---
+++ src/main/java/net/ggmd/teamprojectmod/block/ModBlocks.java
@@ -0,0 +1,15 @@
+package net.ggmd.teamprojectmod.block;
+
+import net.ggmd.teamprojectmod.TeamProjectMod;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TeamProjectMod.MOD_ID);
+
+
+
+
+}
---
+++ src/main/java/net/ggmd/teamprojectmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.ggmd.teamprojectmod.item;
+
+import net.ggmd.teamprojectmod.TeamProjectMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TeamProjectMod.MOD_ID);
+
+    public static final RegistryObject<Item> RAW_BRICK = ITEMS.register("raw_brick",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
